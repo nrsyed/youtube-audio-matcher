29844bca604e0051c04725a383262e40a8584f25
Najam Syed <najam.r.syed@gmail.com>
2020-10-11 14:38:15 -0400

    Add new functions to __init__.py

--- youtube_audio_matcher/audio/__init__.py
+++ youtube_audio_matcher/audio/__init__.py
@@ -1,9 +1,9 @@
 from .audio import (
-    find_peaks_2d, fingerprint, get_spectrogram, hash_file, plot_spectrogram,
-    read_file
+    find_peaks_2d, fingerprint, get_spectrogram, hash_file, hash_peaks,
+    plot_peaks, plot_spectrogram, read_file
 )
 
 __all__ = [
     "find_peaks_2d", "fingerprint", "get_spectrogram", "hash_file",
-    "plot_spectrogram", "read_file",
+    "hash_peaks", "plot_peaks", "plot_spectrogram", "read_file",
 ]
