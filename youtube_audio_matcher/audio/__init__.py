6e91bc5b09697beb31239aedf6a8c3c0d182b86d
Najam Syed <najam.r.syed@gmail.com>
2020-10-17 15:27:59 -0400

    Update __init__.py to match new function names

--- youtube_audio_matcher/audio/__init__.py
+++ youtube_audio_matcher/audio/__init__.py
@@ -1,9 +1,11 @@
 from .audio import (
-    find_peaks_2d, fingerprint, get_spectrogram, hash_file, hash_peaks,
-    plot_peaks, plot_spectrogram, read_file
+    find_peaks_2d, fingerprint_from_file, fingerprint_from_signal,
+    get_spectrogram, hash_file, hash_peaks, plot_peaks, plot_spectrogram,
+    read_file,
 )
 
 __all__ = [
-    "find_peaks_2d", "fingerprint", "get_spectrogram", "hash_file",
-    "hash_peaks", "plot_peaks", "plot_spectrogram", "read_file",
+    "find_peaks_2d", "fingerprint_from_file", "fingerprint_from_signal",
+    "get_spectrogram", "hash_file", "hash_peaks", "plot_peaks",
+    "plot_spectrogram", "read_file",
 ]
