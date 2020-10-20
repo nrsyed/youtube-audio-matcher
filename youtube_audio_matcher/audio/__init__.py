7d3027734e6a28bc4d7b5b9c71e7af96171ef7b0
Najam Syed <najam.r.syed@gmail.com>
2020-10-20 19:58:01 -0400

    Add async function to __init__.py

--- youtube_audio_matcher/audio/__init__.py
+++ youtube_audio_matcher/audio/__init__.py
@@ -1,6 +1,7 @@
 from .fingerprint import (
     find_peaks_2d, fingerprint_from_file, fingerprint_from_signal,
-    get_spectrogram, hash_peaks, plot_peaks, plot_spectrogram
+    fingerprint_songs, get_spectrogram, hash_peaks, plot_peaks,
+    plot_spectrogram, 
 )
 
 from .util import generate_waveform, hash_file, read_file
@@ -8,6 +9,6 @@ from .util import generate_waveform, hash_file, read_file
 
 __all__ = [
     "find_peaks_2d", "fingerprint_from_file", "fingerprint_from_signal",
-    "get_spectrogram", "hash_peaks", "plot_peaks", "plot_spectrogram",
-    "generate_waveform", "hash_file", "read_file"
+    "fingerprint_songs", "get_spectrogram", "hash_peaks", "plot_peaks",
+    "plot_spectrogram", "generate_waveform", "hash_file", "read_file",
 ]
