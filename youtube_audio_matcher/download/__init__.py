13eace18e96abcf130c1e75b41d8d33306bd8529
Najam Syed <najam.r.syed@gmail.com>
2020-10-29 19:53:09 -0400

    Add video_data_from_url function to module namespace

--- youtube_audio_matcher/download/__init__.py
+++ youtube_audio_matcher/download/__init__.py
@@ -1,11 +1,11 @@
 from .download import (
     download_channels, download_video_mp3, download_video_mp3s, get_source,
     get_videos_page_url, run_download_channels, video_metadata_from_source,
-    video_metadata_from_urls,
+    video_metadata_from_url, video_metadata_from_urls,
 )
 
 __all__ = [
     "download_channels", "download_video_mp3", "download_video_mp3s", "get_source",
     "get_videos_page_url", "run_download_channels", "video_metadata_from_source",
-    "video_metadata_from_urls",
+    "video_metadata_from_url", "video_metadata_from_urls",
 ]
